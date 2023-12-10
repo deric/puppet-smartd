aef77b3434c8a65e1c948315e548258086ecd518
Tomas Barton <barton.tomas@gmail.com>
2023-12-10 18:19:36 +0100

    Service has different name on each os

--- spec/acceptance/smartd_spec.rb
+++ spec/acceptance/smartd_spec.rb
@@ -10,17 +10,27 @@ describe 'smartd' do
         include smartd
       EOS
 
-      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_failures: false)
       apply_manifest(pp, catch_changes: true)
     end
 
-    describe file('/etc/smartd.conf') do
+    if os[:family] == 'debian'
+      service = 'smartmontools'
+      config = '/etc/smartd.conf'
+    elsif os[:family] == 'redhat'
+      service = 'smartd'
+      config = '/etc/smartmontools/smartd.conf'
+    else
+      puts os[:family]
+    end
+
+    describe file(config) do
       it { is_expected.to be_file }
       it { is_expected.to be_readable.by('owner') }
       it { is_expected.to be_readable.by('group') }
     end
 
-    describe service('smartmontools') do
+    describe service(service) do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
